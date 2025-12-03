7b83d97af9691eaf9f362df3397452b52ddca75f
Lily Ru <yru@jh.edu>
2025-12-02 21:47:32 -0500

    implemented main functions

--- csf_assign05/message_queue.cpp
+++ csf_assign05/message_queue.cpp
@@ -4,18 +4,28 @@
 
 MessageQueue::MessageQueue() {
   // TODO: initialize the mutex and the semaphore
-  
+  pthread_mutex_init(&m_lock, nullptr);
+  sem_init(&m_avail, 0, 0);
 }
 
 MessageQueue::~MessageQueue() {
   // TODO: destroy the mutex and the semaphore
+  pthread_mutex_destroy(&m_lock);
+  sem_destroy(&m_avail);
 }
 
 void MessageQueue::enqueue(Message *msg) {
   // TODO: put the specified message on the queue
+  // lock while modifying queue
+  pthread_mutex_lock(&m_lock);
+
+  m_messages.push_back(msg);
+
+  pthread_mutex_unlock(&m_lock);
 
   // be sure to notify any thread waiting for a message to be
   // available by calling sem_post
+  sem_post(&m_avail);
 }
 
 Message *MessageQueue::dequeue() {
@@ -32,8 +42,17 @@ Message *MessageQueue::dequeue() {
 
   // TODO: call sem_timedwait to wait up to 1 second for a message
   //       to be available, return nullptr if no message is available
+  if (sem_timedwait(&m_avail, &ts) == -1) {
+    return nullptr;
+  }
 
   // TODO: remove the next message from the queue, return it
   Message *msg = nullptr;
+  if (!m_messages.empty()) {
+    msg = m_messages.front();
+    m_messages.pop_front();
+  }
+  
+  pthread_mutex_unlock(&m_lock);
   return msg;
 }
--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -19,6 +19,13 @@
 
 // TODO: add any additional data types that might be helpful
 //       for implementing the Server member functions
+struct ClientInfo {
+  //Used to pass multiple arguments to client handler thread
+  Connection *conn;
+  Server *server;
+  
+  ClientInfo(Connection *c, Server *s) : conn(c), server(s) {}
+};
 
 ////////////////////////////////////////////////////////////////////////
 // Client thread functions
@@ -32,14 +39,40 @@ void *worker(void *arg) {
   // TODO: use a static cast to convert arg from a void* to
   //       whatever pointer type describes the object(s) needed
   //       to communicate with a client (sender or receiver)
+  ClientInfo *info = static_cast<ClientInfo *>(arg);
+  Connection *conn = info->conn;
+  Server *server = info->server;
 
   // TODO: read login message (should be tagged either with
   //       TAG_SLOGIN or TAG_RLOGIN), send response
+  Message login_msg;
+  if (!conn->receive(login_msg)) {
+    delete info;
+    delete conn;
+    return nullptr;
+  }
 
   // TODO: depending on whether the client logged in as a sender or
   //       receiver, communicate with the client (implementing
   //       separate helper functions for each of these possibilities
   //       is a good idea)
+  if (login_msg.tag == TAG_SLOGIN) {
+    // Handle sender login
+    conn->send(Message(TAG_OK, "logged in as sender"));
+    chat_with_sender(info); //helper function to handle sender
+  } else if (login_msg.tag == TAG_RLOGIN) {
+    // Handle receiver login
+    // (Implementation of receiver handling would go here)
+    conn->send(Message(TAG_OK, "logged in as receiver"));
+    chat_with_receiver(info); //helper function to handle receiver
+  } else {
+    // Invalid login tag
+    conn->send(Message(TAG_ERR, "invalid login"));
+  }
+
+  // Cleanup
+  delete conn;
+  delete info;
 
   return nullptr;
 }
@@ -54,23 +87,59 @@ Server::Server(int port)
   : m_port(port)
   , m_ssock(-1) {
   // TODO: initialize mutex
+  pthread_mutex_init(&m_lock, nullptr);
 }
 
 Server::~Server() {
   // TODO: destroy mutex
+  pthread_mutex_destroy(&m_lock);
 }
 
 bool Server::listen() {
   // TODO: use open_listenfd to create the server socket, return true
   //       if successful, false if not
+  m_ssock = open_listenfd(std::to_string(m_port).c_str());
+  return m_ssock != -1;
 }
 
 void Server::handle_client_requests() {
   // TODO: infinite loop calling accept or Accept, starting a new
   //       pthread for each connected client
+  while (true) {
+    // Accept a new client connection
+    int client_fd = accept(m_ssock, nullptr, nullptr);
+    // Handle error if accept fails
+    if (client_fd == -1) {
+      std::cerr << "Error accepting connection\n";
+      continue;
+    }
+    // Create Connection and ClientInfo objects
+    Connection *conn = new Connection(client_fd);
+    ClientInfo *info = new ClientInfo(conn, this);
+
+    // Create a new thread to handle the client
+    pthread_t thread_id;
+    // handle the error if pthread_create fails
+    if (pthread_create(&thread_id, nullptr, worker, info) != 0) {
+      std::cerr << "Error creating thread\n";
+      delete conn;
+      delete info;
+    }
+  }
 }
 
+//Critical Section of Synchronized Room Creation
 Room *Server::find_or_create_room(const std::string &room_name) {
   // TODO: return a pointer to the unique Room object representing
   //       the named chat room, creating a new one if necessary
+  Guard guard(m_lock);
+  // check if room exists
+  auto it = m_rooms.find(room_name);
+  if (it != m_rooms.end()) {
+    return it->second; //if so, return existing room
+  } else { //if not, create a new room
+    Room *new_room = new Room(room_name);
+    m_rooms[room_name] = new_room;
+    return new_room;
+  }
 }
